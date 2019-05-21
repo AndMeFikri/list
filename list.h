48f1035ac279d91cd9fbd6bfa5ceffcd03abe00c
AndMeFikri <49359822+AndMeFikri@users.noreply.github.com>
2019-05-21 13:36:16 +1000

    Update list.h

--- list.h
+++ list.h
@@ -1,4 +1,13 @@
 #ifndef list
 	#define list
 
+typedef struct roll {
+	struct roll *next;
+	void *data;
+} *roll;
+
+void roll_destruct(roll);
+int roll_prepend(void *);
+void *roll_fetch(roll *);
+
 #endif
